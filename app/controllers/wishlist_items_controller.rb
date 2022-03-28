7ab071716d42b82da4768f719e26e4ce0336e3ed
Hannah Glazier <hrayglazier@aol.com>
2022-03-28 16:33:49 -0600

    initial wishlist controller setup

--- app/controllers/wishlist_items_controller.rb
+++ app/controllers/wishlist_items_controller.rb
@@ -1,3 +1,19 @@
 class WishlistItemsController < ApplicationController
     
+    get '/wishlist_items' do 
+        wishlist = Wishlist_item.all
+        wishlist.to_json(
+            only: [:name],
+            include: {user: {only: [:name]}}
+        )
+    end
+
+    get '/wishlist_item/:id' do
+        wishlist = Wishlist_item.find(params[:id])
+        wishlist.to_json(
+            only: [:name],
+            include: {user: {only: [:name]}}
+        )
+    end
+
 end
